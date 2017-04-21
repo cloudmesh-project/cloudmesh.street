50e36e70d2e099ef22101a54d33343b6cef4a8d9
Rahul Raghatate <rvraghatate10@gmail.com>
2017-04-21 18:33:34 -0400

    fix:updated spark job and dependencies

--- ansible/roles/analysis/files/code/signdetectionbyspark.py
+++ ansible/roles/analysis/files/code/signdetectionbyspark.py
@@ -8,6 +8,7 @@ import numpy
 import sys
 
 def process_images(image):
+		import cv2
 		stop_cascade = cv2.CascadeClassifier('/opencv_workspace/classifier/stopsign_classifier.xml')
 		test = cv2.imread(image)
 		gray = cv2.cvtColor(test,cv2.COLOR_BGR2GRAY)
@@ -26,15 +27,17 @@ if __name__== "__main__":
 	sc = SparkContext(conf = conf)
 	imageDir = sys.argv[1]
 	outputDir = sys.argv[2]
-        print(imageDir)
-        print(outputDir)
-        print(paths.list_images(imageDir))       
+        #print(imageDir)
+        #print(outputDir)
+        #print(paths.list_images(imageDir))       
 	pd = sc.parallelize(paths.list_images(imageDir))
 	pdc = pd.map(process_images)
 	result = pdc.collect()
 	count = 1
 	for img in result:
 		# show objects on street image
-		plt.figure()
-	        plt.axis("off")
-                plt.imsave(outputDir+str(count)+'.jpg',cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+		#plt.figure()
+	        #plt.axis("off")
+                #plt.imsave(outputDir+str(count)+'.jpg',cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+		cv2.imwrite(outputDir+str(count)+'.jpg',img)
+                count = count+1
