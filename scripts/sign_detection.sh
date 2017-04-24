8a3aedb269948a21002e5a67db88f772393991a0
Rahul Raghatate <rvraghatate10@gmail.com>
2017-04-24 07:55:11 -0400

    chg:benchmark update

--- scripts/sign_detection.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Change Directory for playbook_run:
-
-cd $HOME/github/cloudmesh.street/ansible
-
-#Run ansible playbook:
-
-ansible-playbook sign_detection.yaml -i inv --ask-sudo-pass -vvv
